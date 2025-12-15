7d923da5d534338829058cd09ca8a99f5747ea4f
mendess <pedro.mendes.26@gmail.com>
2025-12-15 20:14:50 +0000

    Add death stats

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# inspiration: https://dev.to/rogertorres/first-steps-with-docker-rust-30oi
+
+FROM rust:1.92-bookworm AS build
+
+# create an empty shell project
+RUN USER=root cargo new --bin mc-frontend
+WORKDIR /blind-eternities
+
+# copy manifests
+COPY ./Cargo.lock ./Cargo.lock
+COPY ./Cargo.toml ./Cargo.toml
+RUN cargo build --release
+RUN rm -r ./src
+
+# copy real source
+COPY ./src ./src
+COPY ./templates ./templates
+
+# build for release
+RUN rm ./target/release/mc-frontend*
+RUN find ./src -name '*rs' -exec touch '{}' \;
+RUN cargo build --release
+
+# executing image
+FROM debian:bookworm-slim
+
+COPY --from=build /mc-frontend/target/release/mc-frontend mc-frontend
+#COPY ./assets ./assets
+
+ENTRYPOINT ["./mc-frontend"]
---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use askama::Template;
+use axum::{
+    Router,
+    extract::State,
+    http::StatusCode,
+    response::{Html, IntoResponse},
+    routing::get,
+};
+use serde::Deserialize;
+use std::{fs::File, io, path::PathBuf, sync::Arc};
+
+#[derive(Deserialize)]
+struct Config {
+    backups_dir: PathBuf,
+}
+
+fn get_configuration() -> Result<Config, config::ConfigError> {
+    config::Config::builder()
+        .add_source(config::File::with_name("config/conf").required(false))
+        .build()
+        .and_then(config::Config::try_deserialize)
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let config = get_configuration()?;
+    let router = Router::new()
+        .route("/", get(index))
+        .route("/deaths", get(deaths))
+        .with_state(Arc::new(config));
+
+    println!("serving at http://localhost:50002");
+    axum::serve(
+        tokio::net::TcpListener::bind("0.0.0.0:50002").await?,
+        router,
+    )
+    .await?;
+    Ok(())
+}
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("io: {0}")]
+    Io(#[from] io::Error),
+    #[error("serde: {0}")]
+    Serde(#[from] serde_json::Error),
+    #[error("rendering: {0}")]
+    Rendering(#[from] askama::Error),
+}
+
+impl IntoResponse for Error {
+    fn into_response(self) -> axum::response::Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.to_string()).into_response()
+    }
+}
+
+#[derive(Debug, Template)]
+#[template(path = "deaths/index.html")]
+struct Deaths {
+    deaths: Vec<(String, String, String)>,
+}
+
+#[derive(Debug, Template)]
+#[template(path = "index.html")]
+struct Index;
+
+async fn index() -> Result<impl IntoResponse, Error> {
+    Ok(Html(Index.render()?))
+}
+
+async fn deaths(state: State<Arc<Config>>) -> Result<impl IntoResponse, Error> {
+    let deaths = serde_json::from_reader(File::open(state.backups_dir.join("deaths.json"))?)?;
+    Ok(Html(Deaths { deaths }.render()?))
+}
