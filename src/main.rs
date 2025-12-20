d10c690637a8e1c260fd0ac1ddee361f4d015506
mendess <pedro.mendes.26@gmail.com>
2025-12-20 00:23:59 +0000

    Add modlist page

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,22 @@ use axum::{
     response::{Html, IntoResponse, Redirect},
     routing::get,
 };
+use futures::TryStreamExt;
+use regex::Regex;
 use serde::Deserialize;
-use std::{fs::File, io, path::PathBuf, sync::Arc};
+use std::{
+    fs::File,
+    io,
+    path::PathBuf,
+    sync::{Arc, LazyLock},
+};
+use tokio_stream::wrappers::ReadDirStream;
 use tower_http::services::ServeDir;
 
 #[derive(Deserialize)]
 struct Config {
     backups_dir: PathBuf,
+    server_dir: PathBuf,
 }
 
 fn get_configuration() -> Result<Config, config::ConfigError> {
@@ -28,6 +37,7 @@ async fn main() -> anyhow::Result<()> {
     let router = Router::new()
         .route("/", get(index))
         .route("/deaths", get(deaths))
+        .route("/mods", get(get_mods))
         .route("/super-secret-map", get(Redirect::to("/super-secret-map/")))
         .nest_service(
             "/super-secret-map/",
@@ -78,4 +88,91 @@ async fn index() -> Result<impl IntoResponse, Error> {
 async fn deaths(config: State<Arc<Config>>) -> Result<impl IntoResponse, Error> {
     let deaths = serde_json::from_reader(File::open(config.backups_dir.join("deaths.json"))?)?;
     Ok(Html(Deaths { deaths }.render()?))
+}
+
+#[derive(Debug, Default, Template)]
+#[template(path = "mods/index.html")]
+struct Mods {
+    required: Vec<Mod>,
+    recommended: Vec<Mod>,
+    client_side: Vec<Mod>,
+}
+
+#[derive(Debug, Clone, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
+struct Mod {
+    name: String,
+    version: String,
+}
+
+async fn get_mods(config: State<Arc<Config>>) -> Result<impl IntoResponse, Error> {
+    static MOD_PARSER: LazyLock<Regex> =
+        LazyLock::new(|| Regex::new(r"(?<name>[a-zA-Z\-]+)-(?<version>.*)\.jar").unwrap());
+    let mods_dir = ReadDirStream::new(tokio::fs::read_dir(config.server_dir.join("mods")).await?)
+        .try_collect::<Vec<_>>()
+        .await?;
+
+    const SERVER_ONLY_MODS: &[&str] = &["Prometheus-Exporter", "servercore"];
+
+    const MANDATORY_MODS: &[&str] = &["create", "copycats", "simple-voice-chat"];
+
+    static CLIENT_SIDE_MODS: LazyLock<Vec<Mod>> = LazyLock::new(|| {
+        vec![
+            Mod {
+                name: "mouse-tweaks".into(),
+                version: "any".into(),
+            },
+            Mod {
+                name: "sodium".into(),
+                version: "any".into(),
+            },
+            Mod {
+                name: "lithium".into(),
+                version: "any".into(),
+            },
+        ]
+    });
+
+    let mut mods = mods_dir
+        .into_iter()
+        .filter_map(|s| {
+            let p = s.path();
+            let p = p.file_name().unwrap();
+            let m = p.to_str().unwrap();
+            let captures = MOD_PARSER.captures(m).unwrap();
+            let name = captures.name("name").unwrap();
+            let version = captures.name("version").unwrap();
+            if SERVER_ONLY_MODS.contains(&name.as_str()) {
+                return None;
+            }
+            let m = Mod {
+                name: match name.as_str() {
+                    "voicechat-neoforge" => "simple-voice-chat",
+                    "no-chat-reports-NeoForge" => "no-chat-reports",
+                    n => n,
+                }
+                .to_owned(),
+                version: version.as_str().to_owned(),
+            };
+            if MANDATORY_MODS.contains(&m.name.as_str()) {
+                Some(Either::Left(m))
+            } else {
+                Some(Either::Right(m))
+            }
+        })
+        .fold(Mods::default(), |mut acc, m| {
+            match m {
+                Either::Left(m) => acc.required.push(m),
+                Either::Right(m) => acc.recommended.push(m),
+            }
+            acc
+        });
+    mods.required.sort();
+    mods.recommended.sort();
+    mods.client_side = CLIENT_SIDE_MODS.clone();
+    Ok(Html(mods.render()?))
+}
+
+enum Either<T> {
+    Left(T),
+    Right(T),
 }
